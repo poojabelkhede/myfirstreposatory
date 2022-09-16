feda201dea99e00e59b6502470f6cc0d04c12902
poojabelkhede <110652890+poojabelkhede@users.noreply.github.com>
2022-09-16 13:34:52 +0530

    Add files via upload

---
+++ stutant tsv data prac2 (1).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd()
+
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+pd.read_csv("Student.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
